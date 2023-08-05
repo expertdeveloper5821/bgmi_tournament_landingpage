6e9ea1be5ea89ac0af0db9e7d4ca292f45a2c1ea
expertdeveloper5821 <128484693+expertdeveloper5821@users.noreply.github.com>
2023-08-06 00:28:29 +0530

    Update index.js for console

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 var countDownDate = new Date("Aug 15, 2023 15:37:25").getTime();
-console.log("%cWelcome to pattseheadshot", "color: green;", "CAPSCODE")
+console.log("%cWelcome to pattseheadshot", "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113); margin-bottom: 12px; padding: 5%")
 var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
