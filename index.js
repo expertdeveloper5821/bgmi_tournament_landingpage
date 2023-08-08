f0488d2862d9953ec420975e076999bf8a47e466
Sumit987653 <sumitthapa@technogetic.com>
2023-08-08 19:17:29 +0530

    animations

--- index.js
+++ index.js
@@ -20,9 +20,12 @@ let excitingTournment = document.getElementById("excitingTournment");
 let seamless = document.getElementById("seamless");
 let cashPrize = document.getElementById("cashPrize");
 let skills = document.getElementById("skills");
-
 let dropBoxSection = document.getElementById("drop-box-section");
 let dropBox = document.getElementById("drop-box");
+let change = document.getElementById("change");
+let singup = document.getElementById("singup");
+let play = document.getElementById("play");
+let rewards = document.getElementById("rewards");
 
 clock.addEventListener("mouseenter", () => {
   changeText.innerText =
@@ -48,6 +51,28 @@ skills.addEventListener("mouseenter", () => {
   changeText.innerText =
     "Showcase your gaming prowess in skill-based challenges designed to test your strategic thinking and reflexes.";
 });
+
+
+singup.addEventListener("mouseenter", () => {
+  change.innerText =
+    "Create your free account in just a few simple steps and join our ever-growing gaming community.";
+});
+
+
+
+
+play.addEventListener("mouseenter", () => {
+  change.innerText =
+    "Dive into intense BGMI battles, showcase your skills, and climb the leaderboard to win cash rewards";
+});
+
+rewards.addEventListener("mouseenter", () => {
+  change.innerText =
+    "Cash out your earnings with ease and enjoy the real benefits of your gaming talent";
+});
+
+
+
 
 // const timer = document.getElementById("scope_target_timmer");
 // let seconds = 15;
@@ -81,3 +106,16 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 observer.observe(dropBoxSection);
+
+
+
+const divs = document.querySelectorAll('.flex_icon');
+let currentIndex = 1;
+
+divs.forEach((div, index) => {
+  div.addEventListener('click', () => {
+    divs[currentIndex].classList.remove('active');
+    currentIndex = (index + 1) % divs.length;
+    divs[currentIndex].classList.add('active');
+  });
+});
