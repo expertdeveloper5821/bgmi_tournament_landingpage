9e7cb23bca8d270fbe9548faee58bfe2ae66c64d
Expert developer <expertdeveloper@Experts-MacBook-Pro.local>
2023-09-29 00:19:50 +0530

    Updated carousel

--- index.js
+++ index.js
@@ -5,98 +5,77 @@ const WEB_URL_LOCAL = "file://";
 const WEB_URL = "https://pattseheadshot.com/";
 
 var lastTournament;
-fetch(API_URL)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Network response was not ok");
-    }
-  })
 
-  .then((data) => {
-    console.log("API Data:", data);
-    lastTournament = data.pop();
-    const mapImgUrl = lastTournament.mapImg;
-    const bgmiImg = document.querySelector(".bgmi_img img");
-    bgmiImg.src = mapImgUrl;
-    const formattedDateAndTime = moment(lastTournament.dateAndTime).format(
-      "YYYY-MM-DD h:mm a"
-    );
-    var html = `<div>
-        <h2 class="bgmiHead">${lastTournament.gameName}</h2>
-        <p class="bgmi_para">Date And Time: ${formattedDateAndTime}</p>
-        </div>
-        <div class="winning_prize">
-          <div class="winning_sec_left">
-            <p class="winning_Para">WINNING PRIZE &nbsp <button id="openPopupBtn"><i class="fa-solid fa-arrow-down"></i></button></p>
+const renderData = (matchData) => {
+  lastTournament = matchData
+  console.log("match click", matchData)
+  const mapImgUrl = matchData.mapImg;
+  const bgmiImg = document.querySelector(".bgmi_img img");
+  bgmiImg.src = mapImgUrl;
+  const formattedDateAndTime = moment(matchData.dateAndTime).format(
+    "YYYY-MM-DD h:mm a"
+  );
+  var html = `<div>
+      <h2 class="bgmiHead">${matchData.gameName}</h2>
+      <p class="bgmi_para">Date And Time: ${formattedDateAndTime}</p>
+      </div>
+      <div class="winning_prize">
+        <div class="winning_sec_left">
+          <p class="winning_Para">WINNING PRIZE &nbsp <button id="openPopupBtn"><i class="fa-solid fa-arrow-down"></i></button></p>
 
-            <div id="popup" class="popup">
-            <div class="popup-content">
-              <span class="close" id="closePopupBtn">&times;</span>
-           
-              <div class="WinngColor">
+          <div id="popup" class="popup">
+          <div class="popup-content">
+            <span class="close" id="closePopupBtn">&times;</span>
+         
+            <div class="WinngColor">
 
-           <div class="winningSec">
-           <h4 class="WiiningPoolSec">WINNING PRIZE POOL</h4>
-           <p clas="pool">BGMI Squad match</p>
-           </div>
-              <div class="prizePool" id="popupp"></div>
-              </div>
+         <div class="winningSec">
+         <h4 class="WiiningPoolSec">WINNING PRIZE POOL</h4>
+         <p clas="pool">${matchData.gameName}</p>
+         </div>
+            <div class="prizePool" id="popupp"></div>
             </div>
           </div>
-
-          
-            <h3 class="text_size">Last Survival: 200<span> <img src="./assets/rupee img.svg" class="rupee" /></span
-              ></h3>
-          </div>
-          <div class="winning_sec_right">
-            <p class="winning_Para">ENTERY FEES</p>
-            <span class="text_size"
-              >
-              60<span> <img src="./assets/rupee img.svg" class="rupee" /></span
-            ></span>
-          </div>
         </div>
-        <div class="typeandversion">
-          <div class="first">
-            <p class="winning_Para">TYPE</p>
-            <h4 class="text_color">${lastTournament.gameType}</h4>
-          </div>
-          <div class="second">
-            <p class="winning_Para">VERSION</p>
-            <h4 class="text_color">${lastTournament.version}</h4>
-          </div>
-          <div class="third">
-            <p class="winning_Para">MAP</p>
-            <h4 class="text_map">${lastTournament.mapType}</h4>
-          </div>
+
+        
+          <h3 class="text_size">Last Survival: 200<span> <img src="./assets/rupee img.svg" class="rupee" /></span
+            ></h3>
         </div>
-        <div class="spots">
-          <div class="spot_space">
-            <input type="range" class="input_range" disabled />
-            <p class="winning_Para_top">Only 30 spots left 20/50</p>
-          </div>
-          <div class="button_space">
-            <a href="https://app.pattseheadshot.com/auth/signup" target="_blank" class="btn">JOIN</a>
-          </div>
-        </div>`;
+        <div class="winning_sec_right">
+          <p class="winning_Para">ENTERY FEES</p>
+          <span class="text_size"
+            >
+            ${matchData.entryFee}<span> <img src="./assets/rupee img.svg" class="rupee" /></span
+          ></span>
+        </div>
+      </div>
+      <div class="typeandversion">
+        <div class="first">
+          <p class="winning_Para">TYPE</p>
+          <h4 class="text_color">${matchData.gameType}</h4>
+        </div>
+        <div class="second">
+          <p class="winning_Para">VERSION</p>
+          <h4 class="text_color">${matchData.version}</h4>
+        </div>
+        <div class="third">
+          <p class="winning_Para">MAP</p>
+          <h4 class="text_map">${matchData.mapType}</h4>
+        </div>
+      </div>
+      <div class="spots">
+        <div class="spot_space">
+          <input type="range" class="input_range" disabled />
+          <p class="winning_Para_top">Only 30 spots left 20/50</p>
+        </div>
+        <div class="button_space">
+          <a href="https://app.pattseheadshot.com/auth/signup?gameId=${matchData.roomUuid}" target="_blank" class="btn">JOIN</a>
+        </div>
+      </div>`;
 
-    document.getElementById("rooms").innerHTML = html;
-
-    var matches = data;
-
-    var allimg = "";
-    matches.forEach((element) => {
-      allimg += `<img  src="./assets/1.svg" alt="1" />`;
-    });
-    document.getElementById("allImges").innerHTML = allimg;
-  })
-  .catch((error) => {
-    console.error("Fetch error:", error);
-  })
-  .finally(() => {
-    const popUpButton = document.getElementById("openPopupBtn");
+  document.getElementById("rooms").innerHTML = html;
+  const popUpButton = document.getElementById("openPopupBtn");
     const popUp = document.getElementById("popup");
     const closePopUp = document.getElementById("closePopupBtn");
     if (closePopUp) {
@@ -107,14 +86,53 @@ fetch(API_URL)
 
     var popuppp = "";
     popUpButton?.addEventListener("click", () => {
-      popuppp = `<h5>Last Survival: ${lastTournament.lastServival} <span> <img src="./assets/rupee img.svg" class="rupee"></span> </h5>
+      popuppp = `<h5>Last Survival: ${lastTournament.lastSurvival} <span> <img src="./assets/rupee img.svg" class="rupee"></span> </h5>
       <h5>Highest kill: ${lastTournament.highestKill} <span> <img src="./assets/rupee img.svg" class="rupee"></span> </h5>
       <h5>2nd Winner: ${lastTournament.secondWin}  <span> <img src="./assets/rupee img.svg" class="rupee"></span>  </h5>
       <h5>3nd Winner: ${lastTournament.thirdWin} <span> <img src="./assets/rupee img.svg" class="rupee"></span> </h5>`;
       document.getElementById("popupp").innerHTML = popuppp;
       popUp.style.display = "block";
     });
-  });
+}
+fetch(API_URL)
+  .then((response) => {
+    if (response.ok) {
+    return response.json();
+    } else {
+      throw new Error("Network response was not ok");
+    }
+  })
+
+  .then((data) => {
+    console.log("API Data:", data);
+    var roomData = data
+    // lastTournament = roomData[0]
+    
+    renderData(roomData[0])
+    var matches = data;
+
+    var allimg = [];
+    matches.forEach((element) => {
+      console.log("element", element)
+      const imgElm = document.createElement('img')
+      imgElm.src = element?.mapImg ? element?.mapImg : "./assets/1.svg"
+      imgElm.addEventListener('click', () => renderData(element))
+      // allimg += `<div class="carousel_item" onclick="renderData(${element})"><img  src=${element?.mapImg ? element?.mapImg : "./assets/1.svg"} alt="1" onclick="renderData(${element})"/></div>`;
+      allimg.push(imgElm) 
+    });
+    console.log('allImges', allimg)
+    const crslElm = document.getElementById("allImges")
+    if(crslElm) {
+      // crslElm.innerHTML(allimg)
+      allimg.forEach((imgElm) => {
+
+        crslElm.appendChild(imgElm)
+      })
+    }
+  })
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  })
 
 let clock = document.getElementById("clock");
 let changeText = document.getElementById("changing-text");
