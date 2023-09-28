4e7438831e88de42c2a5f2f6ee37403bfe3cc55e
shivangi-gupta123 <124764991+shivangi-gupta123@users.noreply.github.com>
2023-09-28 18:42:00 +0530

    bgmi changes

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const API_URL = "https://gaming-portal-be.vercel.app/api/v1/room/rooms";
-const APP_URL = "https://app.pattseheadshot.com"
-const WEB_URL_WWW = "https://www.pattseheadshot.com/"
-const WEB_URL_LOCAL = "file://"
-const WEB_URL = "https://pattseheadshot.com/"
+const APP_URL = "https://app.pattseheadshot.com";
+const WEB_URL_WWW = "https://www.pattseheadshot.com/";
+const WEB_URL_LOCAL = "file://";
+const WEB_URL = "https://pattseheadshot.com/";
+
 var lastTournament;
 fetch(API_URL)
   .then((response) => {
-
     if (response.ok) {
       return response.json();
     } else {
       throw new Error("Network response was not ok");
     }
   })
+
   .then((data) => {
+    console.log("API Data:", data);
     lastTournament = data.pop();
+    const mapImgUrl = lastTournament.mapImg;
+    const bgmiImg = document.querySelector(".bgmi_img img");
+    bgmiImg.src = mapImgUrl;
+    const formattedDateAndTime = moment(lastTournament.dateAndTime).format(
+      "YYYY-MM-DD h:mm a"
+    );
     var html = `<div>
         <h2 class="bgmiHead">${lastTournament.gameName}</h2>
         <p class="bgmi_para">Time: ${lastTournament.date} at ${lastTournament.time} pm</p>
@@ -80,7 +88,6 @@ fetch(API_URL)
 
     var allimg = "";
     matches.forEach((element) => {
-
       allimg += `<img  src="./assets/1.svg" alt="1" />`;
     });
     document.getElementById("allImges").innerHTML = allimg;
@@ -92,7 +99,7 @@ fetch(API_URL)
     const popUpButton = document.getElementById("openPopupBtn");
     const popUp = document.getElementById("popup");
     const closePopUp = document.getElementById("closePopupBtn");
-    if(closePopUp){
+    if (closePopUp) {
       closePopUp.addEventListener("click", () => {
         popUp.style.display = "none";
       });
@@ -109,7 +116,6 @@ fetch(API_URL)
     });
   });
 
-
 let clock = document.getElementById("clock");
 let changeText = document.getElementById("changing-text");
 let excitingTournment = document.getElementById("excitingTournment");
@@ -123,62 +129,60 @@ let signup = document.getElementById("signup");
 let play = document.getElementById("play");
 let rewards = document.getElementById("rewards");
 
-if(clock) {
+if (clock) {
   clock.addEventListener("mouseenter", () => {
     changeText.innerText =
       "Our dedicated support team is here to assist you around the clock, ensuring a smooth and enjoyable gaming experience.";
   });
 }
 
-if(excitingTournment) {
+if (excitingTournment) {
   excitingTournment.addEventListener("mouseenter", (e) => {
     changeText.innerText =
       "Participate in thrilling tournaments and compete against the best in the community for massive cash prizes and recognition.";
   });
 }
 
-if(seamless){
+if (seamless) {
   seamless.addEventListener("mouseenter", () => {
     changeText.innerText =
       "Our secure payment system ensures hassle-free withdrawals so you can enjoy your rewards without any worries.";
   });
 }
-if(cashPrize) {
+if (cashPrize) {
   cashPrize.addEventListener("mouseenter", () => {
     changeText.innerText =
       "Play your favorite BGMI battles and win real cash rewards. The more you play, the more you earn!";
   });
 }
 
-if(skills) {
+if (skills) {
   skills.addEventListener("mouseenter", () => {
     changeText.innerText =
       "Showcase your gaming prowess in skill-based challenges designed to test your strategic thinking and reflexes.";
   });
 }
 
-if(signup) {
+if (signup) {
   signup.addEventListener("mouseenter", () => {
     change.innerText =
       "Create your free account in just a few simple steps and join our ever-growing gaming community.";
   });
 }
 
-if(play) {
+if (play) {
   play.addEventListener("mouseenter", () => {
     change.innerText =
       "Dive into intense BGMI battles, showcase your skills, and climb the leaderboard to win cash rewards";
   });
 }
 
-if(rewards) {
+if (rewards) {
   rewards.addEventListener("mouseenter", () => {
     change.innerText =
       "Cash out your earnings with ease and enjoy the real benefits of your gaming talent";
   });
 }
-
-
 
 const bodyElem = document.getElementById("body");
 const audio = document.getElementById("audio");
@@ -191,37 +195,39 @@ function playSound() {
 }
 // Attach the onclick event listener to the button
 
-if(bodyElem) {
+if (bodyElem) {
   bodyElem.onclick = playSound;
-  
-  bodyElem.onload = handleLoad;
 
+  bodyElem.onload = handleLoad;
 }
 
-
- 
 function handleLoad() {
   try {
     const search = window.location.search;
-    const localToken = localStorage.getItem('token');
-  
-    if (search.includes('token') || localToken) {
-      if(search.includes('token')) {
-        // Extract the token value from the URL
-        const tokenValue = new URLSearchParams(search).get('token');
-        // Store the token in localStorage
-        localStorage.setItem('token', tokenValue);
-        window.location.href = `${window.location.href.split("?")[0]}`;
+    const localToken = localStorage.getItem("token");
 
+    if (search.includes("token") || localToken) {
+      if (search.includes("token")) {
+        // Extract the token value from the URL
+        const tokenValue = new URLSearchParams(search).get("token");
+        // Store the token in localStorage
+        localStorage.setItem("token", tokenValue);
+        window.location.href = `${window.location.href.split("?")[0]}`;
       }
     } else {
       // Redirect to a different URL
-      if(!(window.location.href === WEB_URL || window.location.href === WEB_URL_WWW || window.location.href === WEB_URL_LOCAL)) {
-        window.location.href = 'https://app.pattseheadshot.com?isLogin=deny';
+      if (
+        !(
+          window.location.href === WEB_URL ||
+          window.location.href === WEB_URL_WWW ||
+          window.location.href === WEB_URL_LOCAL
+        )
+      ) {
+        window.location.href = "https://app.pattseheadshot.com?isLogin=deny";
       }
     }
-  } catch(err) {
-    console.error("In Load => ", err)
+  } catch (err) {
+    console.error("In Load => ", err);
   }
 }
 
